24156adaab3621feab6071067d85d191e8983d7f
David Mitchell <MitcheD1@intnp.wd.govt.nz>
2017-02-26 15:55:58 +1300

    make within_meters look like a const

--- Question4/main.py
+++ Question4/main.py
@@ -4,7 +4,7 @@
 	array of dicts containing staff members names and their distances from `office_point`.
 
 	This output will be sorted (assesnding) by the staff members name and only contain staff members
-	distance from the Storypark home office `office_point` is less than 2000 meters (`within_meters`).
+	distance from the Storypark home office `office_point` is less than 2000 meters (`WITHIN_METERS`).
 
 	It is assumed that the `staff_list.json` file has a `staff` key at it's root which is an array
 	of objects which are assumed to have the structure of:
@@ -22,21 +22,27 @@
 import math
 import json
 import pprint
+import os
+
 from greatcircle import Point
+
+WITHIN_METERS = 2000.0
 
 # define the location of the Storypark offices
 office_point = Point(-41.2920728, 174.7748162)
 
+# the path to the dir of this file
+path = os.path.dirname(os.path.realpath(__file__))
+
 # read in the json file
-with open('staff_list.json') as data_file:
+with open(path+'\staff_list.json') as data_file:
 	data = json.load(data_file)
     
 # get all the staff
 staff = data["staff"]
 
-# this will hold all staff who are within the distance defined in `within_meters`
+# this will hold all staff who are within the distance defined in `WITHIN_METERS`
 close_staff = []
-within_meters = 2000.0
 
 for s in staff:
 	lat = s["location"]["latitute"]
@@ -46,14 +52,14 @@ for s in staff:
 	# find the great circle distance and append the the `close_staff` list
 	# if within the defined distance
 	distance_meters = office_point.distance_to(p)
-	if distance_meters < within_meters:
+	if distance_meters < WITHIN_METERS:
 		close_staff.append( { 'name': s["name"], 'distance': distance_meters } )
 
 # sort `close_staff` by name
 close_staff.sort( key=lambda o: o["name"] )
 
 print ''
-print 'All staff within', within_meters, ' meters'
+print 'All staff within', WITHIN_METERS, ' meters'
 pp = pprint.PrettyPrinter()
 pp.pprint(close_staff)
 print ''
